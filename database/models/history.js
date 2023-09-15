17354d7ccc06b75a98e47979b049f06357f9d814
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2023-09-15 17:57:40 +0300

    Fix .gitignore

---
+++ database/models/history.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const historySchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		history: [{ date: Date, rank: Number, score: Number }],
+	},
+	{ timestamps: true }
+);
+
+const History = mongoose.model("History", historySchema);
+module.exports = History;
